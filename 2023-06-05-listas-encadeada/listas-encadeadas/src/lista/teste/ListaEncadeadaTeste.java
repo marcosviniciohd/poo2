521432a0799906751ceaa29b84ebba934b4c8c2f
Marcos <marcosviniciohd@hotmail.com>
2023-05-12 23:06:37 -0300

    listas encadeadas

--- 2023-06-05-listas-encadeada/listas-encadeadas/src/lista/teste/ListaEncadeadaTeste.java
+++ 2023-06-05-listas-encadeada/listas-encadeadas/src/lista/teste/ListaEncadeadaTeste.java
@@ -1,2 +1,13 @@
-package lista.teste;public class ListaEncadeadaTeste {
+package lista.teste;
+
+import lista.ListaEncadeada;
+
+public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+       lista.adiciona(1);
+
+        System.out.println("Tamanho = " + lista.getTamanho());
+        System.out.println(lista);
+    }
 }
