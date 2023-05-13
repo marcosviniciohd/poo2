660436644450e4a9237bdce718aa57e5de59fc06
Marcos <marcosviniciohd@hotmail.com>
2023-05-12 23:17:24 -0300

    Add elementos na lista

--- 2023-06-05-listas-encadeada/listas-encadeadas/src/lista/ListaEncadeada.java
+++ 2023-06-05-listas-encadeada/listas-encadeadas/src/lista/ListaEncadeada.java
@@ -2,10 +2,18 @@ package lista;
 
 public class ListaEncadeada<T> {
     private No<T> inicio;
+    private No<T> ultimo;
     private int tamanho;
+
     public void adiciona(T elemento){
         No<T> celula = new No<T>(elemento);
+        if(this.tamanho == 0){
+            this.inicio = celula;
+        } else {
+            this.ultimo.setProximo(celula);
+        }
         this.inicio = celula;
+        this.ultimo = celula;
         this.tamanho++;
     }
 
--- 2023-06-05-listas-encadeada/listas-encadeadas/src/lista/teste/ListaEncadeadaTeste.java
+++ 2023-06-05-listas-encadeada/listas-encadeadas/src/lista/teste/ListaEncadeadaTeste.java
@@ -6,7 +6,14 @@ public class ListaEncadeadaTeste {
     public static void main(String[] args) {
         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
        lista.adiciona(1);
+        System.out.println("Tamanho = " + lista.getTamanho());
+        System.out.println(lista);
 
+        lista.adiciona(2);
+        System.out.println("Tamanho = " + lista.getTamanho());
+        System.out.println(lista);
+
+        lista.adiciona(3);
         System.out.println("Tamanho = " + lista.getTamanho());
         System.out.println(lista);
     }
