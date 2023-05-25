953ef755fcd4fae8d4daa42bb524780bf93912d8
Marcos <marcosviniciohd@hotmail.com>
2023-05-25 00:18:09 -0300

    Grupo 5 - Seminário sobre SOLID

---
+++ 2023-19-05-boas-praticas/boas-praticas/src/questao4/Transferencia.java
@@ -0,0 +1,2 @@
+package questao4;public class Transferencia {
+}
---
+++ atividades/seminario/grupo5/grupo5/src/br/edu/iftm/grupo5/model/Cargo.java
@@ -0,0 +1,10 @@
+package br.edu.iftm.grupo5.model;
+
+public enum Cargo {
+
+	ASSISTENTE,
+	ANALISTA,
+	ESPECIALISTA,
+	GERENTE;
+
+}
