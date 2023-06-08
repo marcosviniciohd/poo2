cce1455304a119d92e29bb3002915abb3bd5a912
Marcos <marcosviniciohd@hotmail.com>
2023-06-08 09:13:50 -0300

    Execicio 1 - Princício de Responsabilidade Única

--- 2023-03-06-execicios-solid/principio-responsabilidade-unica/principio-responsabilidade-unica/principio-responsabilidade-unica/src/br/edu/iftm/pru/service/ConfirmacaoEmail.java
+++ 2023-03-06-execicios-solid/principio-responsabilidade-unica/principio-responsabilidade-unica/principio-responsabilidade-unica/src/br/edu/iftm/pru/service/ConfirmacaoEmail.java
@@ -1,2 +1,13 @@
-package br.edu.iftm.pru.service;public class ConfrimacaoEmail {
+package br.edu.iftm.pru.service;
+
+public class ConfirmacaoEmail {
+    private Pagamento pagamento;
+
+    public ConfirmacaoEmail(Pagamento pagamento) {
+        if(pagamento.getPago()) {
+            System.out.println("Email de confirmação enviado para o cliente.");
+        } else {
+            System.out.println("Favor realizar o pagamento!.");
+        }
+    }
 }
--- 2023-03-06-execicios-solid/principio-responsabilidade-unica/principio-responsabilidade-unica/principio-responsabilidade-unica/src/br/edu/iftm/pru/service/Pagamento.java
+++ 2023-03-06-execicios-solid/principio-responsabilidade-unica/principio-responsabilidade-unica/principio-responsabilidade-unica/src/br/edu/iftm/pru/service/Pagamento.java
@@ -1,2 +1,22 @@
-package br.edu.iftm.pru.service;public class Pagamento {
+package br.edu.iftm.pru.service;
+
+public class Pagamento {
+    private CarrinhoCompras carrinhoCompras;
+    private Boolean pago = false;
+
+    public Pagamento(CarrinhoCompras carrinhoCompras) {
+        this.carrinhoCompras = carrinhoCompras;
+    }
+
+    public void pagar() {
+        this.pago = true;
+    }
+
+    public Boolean getPago() {
+        return pago;
+    }
+
+    public CarrinhoCompras getCarrinhoCompras() {
+        return carrinhoCompras;
+    }
 }
