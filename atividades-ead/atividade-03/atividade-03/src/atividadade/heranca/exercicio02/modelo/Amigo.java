45a4e802aa7832a5524d43f0eac71059f972fcc2
Marcos <marcosviniciohd@hotmail.com>
2023-07-07 13:01:52 -0300

    Exercicio 02

--- atividades-ead/atividade-03/atividade-03/src/atividadade/heranca/exercicio02/modelo/Amigo.java
+++ atividades-ead/atividade-03/atividade-03/src/atividadade/heranca/exercicio02/modelo/Amigo.java
@@ -1,2 +1,18 @@
-package atividadade.heranca.exercicio02.modelo;public class Amigo {
+package atividadade.heranca.exercicio02.modelo;
+
+public class Amigo extends Pessoa {
+    private String dataAniversario;
+
+    public Amigo(String nome, Character sexo, Integer idade, String dataAniversario) {
+        super(nome, sexo, idade);
+        this.dataAniversario = dataAniversario;
+    }
+
+    public String getDataAniversario() {
+        return dataAniversario;
+    }
+
+    public void setDataAniversario(String dataAniversario) {
+        this.dataAniversario = dataAniversario;
+    }
 }
--- atividades-ead/atividade-03/atividade-03/src/atividadade/heranca/exercicio02/modelo/Pessoa.java
+++ atividades-ead/atividade-03/atividade-03/src/atividadade/heranca/exercicio02/modelo/Pessoa.java
@@ -1,2 +1,37 @@
-package atividadade.heranca.exercicio02.modelo;public class Pessoa {
+package atividadade.heranca.exercicio02.modelo;
+
+public class Pessoa {
+    private String nome;
+    private Character sexo;
+    private Integer idade;
+
+    public Pessoa(String nome, Character sexo, Integer idade) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Character getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
 }
