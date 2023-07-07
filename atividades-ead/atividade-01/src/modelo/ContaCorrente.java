41a93d7b8b31d0c9a7f58977244beb4929dc683d
Marcos <marcosviniciohd@hotmail.com>
2023-07-07 10:57:25 -0300

    Exercicio 13 OK

--- atividades-ead/atividade-01/src/modelo/ContaCorrente.java
+++ atividades-ead/atividade-01/src/modelo/ContaCorrente.java
@@ -1,2 +1,59 @@
-package modelo;public class ContaCorrente {
+package modelo;
+
+public class ContaCorrente {
+    private String nome;
+    private String senha;
+    private Double saldo;
+
+    public ContaCorrente(String nome, String senha, Double saldo) {
+        this.nome = nome;
+        this.senha = senha;
+        this.saldo = saldo;
+    }
+
+    public void cadastrarSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void alterarSenha(String senhaAntiga, String senhaNova) {
+        if (senhaAntiga.equals(this.senha)) {
+            this.senha = senhaNova;
+        }
+    }
+
+    public void credito (Double valor, String senha) {
+        if (senha.equals(this.senha)) {
+            this.saldo += valor;
+        } else {
+            System.out.println("Senha incorreta!");
+        }
+    }
+
+    public void debito (Double valor, String senha) {
+        if (senha.equals(this.senha)) {
+            this.saldo -= valor;
+        } else {
+            System.out.println("Senha incorreta!");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
 }
--- atividades-ead/atividade-01/src/principal/TestaContaCorrente.java
+++ atividades-ead/atividade-01/src/principal/TestaContaCorrente.java
@@ -1,2 +1,37 @@
-package principal;public class TestaContaCorrente {
+package principal;
+
+import modelo.ContaCorrente;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TestaContaCorrente {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite a senha: ");
+        String senha = sc.nextLine();
+        System.out.print("Digite o saldo: ");
+        Double saldo = sc.nextDouble();
+        ContaCorrente contaCorrente = new ContaCorrente(nome, senha, saldo);
+        System.out.println("Digite a senha para logar: ");
+        String senhaDigitada = sc.next();
+        if (senhaDigitada.equals(contaCorrente.getSenha())) {
+            System.out.println("Digite o valor para crédito: ");
+            Double valor = sc.nextDouble();
+            contaCorrente.credito(valor, senhaDigitada);
+            System.out.println("Digite o valor para débito: ");
+            valor = sc.nextDouble();
+            contaCorrente.debito(valor, senhaDigitada);
+        } else {
+            System.out.println("Senha incorreta!");
+        }
+        System.out.println("Nome: " + contaCorrente.getNome());
+        System.out.println("Senha: " + contaCorrente.getSenha());
+        System.out.println("Saldo: " + contaCorrente.getSaldo());
+
+
+    }
 }
