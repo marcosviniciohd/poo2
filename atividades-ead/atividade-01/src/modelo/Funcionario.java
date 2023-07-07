e2036dc02a2deaddbf6fc0fef77a1f4a26082347
Marcos <marcosviniciohd@hotmail.com>
2023-07-07 01:40:20 -0300

    Exercicio 7

---
+++ atividades-ead/atividade-01/src/modelo/Funcionario.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Funcionario {
+    private String nome;
+    private Integer horasTrabalhadas;
+    private Double valorHora;
+
+    public Funcionario(String nome, Integer horasTrabalhadas, Double valorHora) {
+        this.nome = nome;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+
+}
