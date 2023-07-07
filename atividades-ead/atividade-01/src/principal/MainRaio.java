4b1babb224844f5094f1ab728e8f4f7be92b88e5
Marcos <marcosviniciohd@hotmail.com>
2023-07-07 01:40:44 -0300

    Exercicio 8

--- atividades-ead/atividade-01/src/principal/MainRaio.java
+++ atividades-ead/atividade-01/src/principal/MainRaio.java
@@ -1,2 +1,17 @@
-package principal;public class MainRaio {
+package principal;
+
+import math.Circulo;
+
+public class MainRaio {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo(32);
+        Circulo circulo1 = new Circulo(44);
+        Circulo circulo2 = new Circulo(17);
+        System.out.println(circulo.calcularArea());
+        System.out.println(circulo1.calcularArea());
+        System.out.println(circulo2.calcularArea());
+        System.out.println(circulo.calcularPerimetro());
+        System.out.println(circulo1.calcularPerimetro());
+        System.out.println(circulo2.calcularPerimetro());
+    }
 }
